ef5162c80f132f2a501052c3b47323a3ec574747
Renato Rodrigues <renato@vulppi.dev>
2024-03-22 14:43:16 -0300

    fix: deep infer levels in i18n

--- src/lib/locales/i18n.ts
+++ src/lib/locales/i18n.ts
@@ -4,17 +4,21 @@ import type { I18NData } from './dictionaries/en-US'
 import { DEFAULT_LOCALE, LOCALES, type Locale } from './dictionaries'
 
 type I18N_Id<E extends Record<string, any>> =
-  E extends Record<infer K, any>
-    ? K extends string
-      ? E[K] extends Record<infer KK, string | object>
-        ? KK extends string
-          ? E[K][KK] extends Record<infer KKK, string>
-            ? KKK extends string
-              ? `${K}.${KK}.${KKK}`
-              : `${K}.${KK}`
-            : `${K}.${KK}`
-          : K
-        : K
+  E extends Record<infer L1, any>
+    ? L1 extends string
+      ? E[L1] extends Record<infer L2, string | object>
+        ? L2 extends string
+          ? E[L1][L2] extends Record<infer L3, string>
+            ? L3 extends string
+              ? E[L1][L2][L3] extends Record<infer L4, string>
+                ? L4 extends string
+                  ? `${L1}.${L2}.${L3}.${L4}`
+                  : `${L1}.${L2}.${L3}`
+                : `${L1}.${L2}.${L3}`
+              : `${L1}.${L2}`
+            : `${L1}.${L2}`
+          : L1
+        : L1
       : never
     : never
 
@@ -25,12 +29,15 @@ type I18N_Options = {
   values?: Record<string, any> | undefined
 }
 
-type TFunction = (id: I18N_Id<I18NData>, opt?: I18N_Options) => string
+type TFunction = (
+  id: I18N_Id<I18NData> | Omit<string, I18N_Id<I18NData>>,
+  opt?: I18N_Options
+) => string
 
 let tFunction: TFunction
 
 export const t = derived<typeof preT, TFunction>(preT, ($t, set) => {
-  set((id, options) => $t(id, options as any))
+  set((id, options) => $t(id as string, options as any))
 })
 
 export const _ = t
